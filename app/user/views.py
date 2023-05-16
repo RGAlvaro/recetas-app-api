9d23f83468947a4a3cf5012a89692573f24c1454
Alvaro Ruiz <ruizgarciaalvaro92@gmail.com>
2023-05-16 17:22:45 +0200

    Implementation of recipe model.

--- app/user/views.py
+++ app/user/views.py
@@ -11,9 +11,6 @@ from user.serializers import (
 )
 
 
-from user.serializers import UserSerializer
-
-
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system."""
     serializer_class = UserSerializer
@@ -23,6 +20,7 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for user."""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user."""
